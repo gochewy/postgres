aa83207629050f7a4253eb26a944489952920f3d
Raphaël Titsworth-Morin <raphael@raphaeltm.com>
2023-03-21 19:02:30 +0000

    postgres deploy switched to async fn

--- .chewy/deployment/index.ts
+++ .chewy/deployment/index.ts
@@ -1,17 +1,18 @@
 import * as pulumi from "@pulumi/pulumi";
 import dev from "./dev";
 
-const stackName = pulumi.getStack();
+export = async () => {
+  const stackName = pulumi.getStack();
+  console.log("@@ stack name: ", stackName);
 
-console.log("@@ stack name: ", stackName);
+  let devOutput: ReturnType<typeof dev> | undefined;
 
-let devOutput: ReturnType<typeof dev> | undefined;
-
-if(stackName === "dev") {
+  if (stackName === "dev") {
     console.log("@@ dev stack");
     devOutput = dev();
-} else if(stackName === "prod") {
+  } else if (stackName === "prod") {
     // prod
-}
+  }
 
-export default devOutput;
+  return devOutput;
+};
